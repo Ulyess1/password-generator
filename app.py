6a2302136b90a7a0b9821dd20aa96873f35dd642
Ulyess1 <hhj3861@gmail.com>
2025-09-30 18:33:43 +0100

    upd

--- app.py
+++ app.py
@@ -2,30 +2,37 @@ import random
 import string
 import csv
 from pathlib import Path
-FILE = Pat(__file__).parent / ""
-def upper(min_length):
- upper_letters = string.ascii_uppercase 
- print(upper_letters)
-def lower(min_length) :
- lower_letters = string.ascii_lowercase
- print(lower_letters)
-def digits(min_length) :
- Numbers = string.digits
- print(Numbers)
-def punctuation(min_length) :
- special = string.punctuation
- print(special)
+FILE = Path(__file__).parent / "save.csv"
+
+def upper():
+  return string.ascii_uppercase
+def lower() :
+  return string.ascii_lowercase
+def digits() :
+  return string.digits
+def special() :
+  return string.punctuation
 
 print("1-upper letters.")
 print("2-lower letters.")
 print("3-Numbers.")
 print("4-Special charahcters.")
+print("0-Exit")
 choice = input("Select Your Choice : ")
 if choice == "1" :
- upper(1)
-if choice == "2" :
- lower(1)
-if choice == "3" :
- digits(1)
-if choice == "4" :
- punctuation(1)
+  value = upper()
+elif choice == "2" :
+  value = lower()
+elif choice == "3" :
+ value = digits()
+elif choice == "4" :
+ value = special()
+elif choice == "0":
+  print("Good Bye..")
+  value = None 
+  
+if value is not None :
+ print(value)
+ with open(FILE, "a", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow([value,])
